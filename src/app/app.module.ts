a2c7ceefa159c38215d0bc9e75ce9a30fd84b872
swetha0 <gaali.swetha@gmail.com>
2020-06-08 16:28:06 -0400

    FSJ0420A104

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BioComponent } from './bio/bio.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BioComponent,
+    PortfolioComponent,
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
